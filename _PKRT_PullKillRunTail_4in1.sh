8a95e2c7ca4357e4a9c78e49c27292c17f4425ed
bingjiw <391904577@qq.com>
2024-07-31 08:19:31 +0800

    fix: 有时会有2个正在运行的 C-o-W 进程ID，如果刚才杀了一个，若还有则还要再杀一个

--- _PKRT_PullKillRunTail_4in1.sh
+++ _PKRT_PullKillRunTail_4in1.sh
@@ -4,9 +4,7 @@ git pull https://github.com/bingjiw/C-o-W master
 
 
 echo "显示当前正在运行的 C-o-W 进程ID，然后用 kill 进程ID 杀掉"
-
 ps -ef | grep app.py | grep -v grep
-
 pid=$(ps -ef | grep "[a]pp.py" | awk '{print $2}')
 if [ -n "$pid" ]; then
     echo "当前运行的 app.py 进程ID: $pid"
@@ -17,6 +15,21 @@ if [ -n "$pid" ]; then
 else
     echo "未找到正在运行的 app.py 进程"
 fi
+
+
+echo "有时会有2个正在运行的 C-o-W 进程ID，如果刚才杀了一个，若还有则还要再杀一个"
+ps -ef | grep app.py | grep -v grep
+pid=$(ps -ef | grep "[a]pp.py" | awk '{print $2}')
+if [ -n "$pid" ]; then
+    echo "当前运行的 app.py 进程ID: $pid"
+    echo "确定要 kill 以上 app.py 进程吗？按回车键将执行 kill 命令，按 Ctrl+C 取消"
+    read
+    kill $pid
+    echo "已尝试终止进程 $pid"
+else
+    echo "未找到正在运行的 app.py 进程"
+fi
+
 
 echo "# 获取当前日期时间"
 current_datetime=$(date +"%Y%m%d_%H%M%S")
